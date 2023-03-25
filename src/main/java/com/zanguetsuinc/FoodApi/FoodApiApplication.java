d0b3a687d875f73c0e1b6926f76709fa00186c25
Jenilson Viwaldo Francisco <franciscoviwaldo@gmail.com>
2023-03-25 21:57:29 +0100

    Ficheiros do projecto adicionados

---
+++ src/main/java/com/zanguetsuinc/FoodApi/FoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.zanguetsuinc.FoodApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zanguetsuinc/FoodApi/FoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanguetsuinc.FoodApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
